77883d76355418fa466531401b6b08b37da36b51
XavierVG16 <kjaviervelasque@hotmail.com>
2023-07-08 15:37:31 -0500

    'login'

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,50 +3,44 @@
 @section('content')
 
    
-    <div class="titulo">
-    <br>
-    <br>
-    
-   <h1 style='color:black'>DEPARTAMENTO DE ENFERMERIA</h1>
-      
-    </div>
-    <div class="titulo">
-    <h2 style='color:black'>INICIAR SECIÓN</h2>
-        <br>
-    </div>
-    <form method="POST"  class="col-3 login" action="{{ route('login') }}">
+<div class="row justify-content-center">
+          <div class="col-lg-8">
+            <div class="card-group d-block d-md-flex row">
+              <div class="card col-md-7 p-4 mb-0">
+                <div class="card-body">
+               
+                        <h1>Login</h1>
+                  <p class="text-medium-emphasis">Departamento de Enfermeria</p>
+                  <form method="POST"  action="{{ route('login') }}">
                         @csrf
+                  <div class="input-group mb-3"><span class="input-group-text">
+                      <svg class="icon">
+                        <use xlink:href="{{ asset('admin/node_modules/@coreui/icons/sprites/free.svg#cil-user') }}"></use>
+                      </svg></span>
+                      <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" placeholder="e-mail" autofocus>
 
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Direccion de correo electronico') }}</label>
+                        @error('email')
+                             <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror                 
+                   </div>
+                  <div class="input-group mb-4"><span class="input-group-text">
+                      <svg class="icon">
+                        <use xlink:href="{{ asset('admin/node_modules/@coreui/icons/sprites/free.svg#cil-lock-locked') }}"></use>
+                      </svg></span>
+                      <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password" placeholder="password">
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                        @error('password')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror                                         
+                    </div>
+                  <div class="row">
 
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <div class="col-md-6 offset-md-4">
+                  <div class="row mb-3">
+                            <div class="col-6 ">
                                 <div class="form-check">
                                     <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
 
@@ -55,22 +49,35 @@
                                     </label>
                                 </div>
                             </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                    </div>
+                    <div class="col-6">
+                    <button type="submit" class="btn btn-primary">
                                     {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
+                    </button>
+                    </div>
+                    <div class="col-6 text-end">
+                    @if (Route::has('password.request'))
                                     <a class="btn btn-link" href="{{ route('password.request') }}">
                                         {{ __('Forgot Your Password?') }}
                                     </a>
                                 @endif
-                            </div>
-                        </div>
-                    </form>
+                    </div>
+                  </div>
+                </form>
+                </div>
+              </div>
+              <div class="card col-md-5 text-white bg-primary py-5">
+                <div class="card-body text-center">
+                  <div>
+                    <h2>Sign up</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    <button class="btn btn-lg btn-outline-light mt-3" type="button">Register Now!</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
     
  
    
--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -1,85 +1,18 @@
-<type html>
+<!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-<link rel="stylesheet" href="{{ asset('admin/css/rstilo.css') }}">
-<style>
-    *{
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-}
-.fondo{
-    min-height: 100vh;
-}
-.fondo_menu{
-    background-color: #212529;
-}
-.boton{
-    color: white;
-    border: 1px solid white;
-    margin-left: 15px;
-    text-decoration: none;
-    padding: 6px 8px;
-    border-radius: 8px;
-    transition: .2s;
-}
-.boton:hover{
-    border: 1px solid gray;
-    color: gray;
-}
-.fondo-login{
-    height: 400px;
-    width: 100%;
-    background-image: url(https://img.freepik.com/vector-gratis/antecedentes-medicos-limpios_53876-97927.jpg?w=2000);
-    background-size: cover;
-  background-repeat:no-repeat;
-  background-position: center center;
-}
-.login{
-    color: white;
-    margin: auto;
-    border: 1px solid gray;
-    padding: 20px;
-    border-radius: 6px;
-    background-color: rgb(51, 47, 47);
-}
-.icon, .titulo{
-    text-align: center;
-    color: white;
-}
-.titulo{
-    margin-bottom: 12px;
-}
-.icon .dog-icon{
-    font-size: 50px;
-    margin-bottom: 10px;
-    color: white;
-}
-.box-eye{
-    position: relative;
-}
-.box-eye button{
-    border: none;
-    position: absolute;
-}
-.changePassword{
-    position: absolute;
-    top: 12px;
-    left: 270px;
-    color: rgb(51, 47, 47);
-    cursor: pointer;
-}
-</style>
-</head>
+@section('htmlheader')
+@include('partials.htmlheader')
+@show
 <body>
-    <div id="app" class='fondo-login' >
+    <div id="app" class="bg-light min-vh-100 d-flex flex-row align-items-center">
    
 
 
-
-        <main class="py-4">
+        <div class="container">
+            <main class="py-4">
             @yield('content')
-        </main>
+            </main>
+        </div>
     </div>
 </body>
 @include('partials.scripts')
